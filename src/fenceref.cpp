d36a126229a03060a6307ba284fe2812a3988c88
Blaine Pace <blainepace@gmail.com>
2013-03-26 01:46:52 -0700

    Mapped pins and tested analog functions
    Mapped LED, signal and measurement pins to variables. Added rudimentary analog
    voltage detection that will illuminate the left hit LED.

--- src/fenceref.cpp
+++ src/fenceref.cpp
@@ -4,20 +4,29 @@
   #include "WProgram.h"
 #endif
 
-unsigned int led_lhit = 0;
-unsigned int led_rhit = 2;
-unsigned int led_loff = 1;
-unsigned int led_roff = 3;
-unsigned int led_epee = 11;
-unsigned int led_foil = 12;
-unsigned int led_saber = 13;
+// - Integer value for 3V, for reading an analog voltage
+unsigned signal_3v = 610;
 
-unsigned int weapon_button = 8;
+// # LED pins
+unsigned const led_lhit = 0;
+unsigned const led_rhit = 2;
+unsigned const led_loff = 1;
+unsigned const led_roff = 3;
+unsigned const led_epee = 11;
+unsigned const led_foil = 12;
+unsigned const led_saber = 13;
 
-unsigned int lhigh = 9;
-unsigned int rhigh = 10;
-unsigned int lgnd = 5;
-unsigned int rgnd = 6;
+unsigned const weapon_button = 8;
+
+// # Signal pins
+unsigned const lhigh = 9;
+unsigned const rhigh = 10;
+unsigned const lgnd = 5;
+unsigned const rgnd = 6;
+
+// # Analog measurement pins
+unsigned const ltip = 0;
+unsigned const rtip = 1;
 
 
 void setup() {
@@ -29,7 +38,20 @@ void setup() {
   pinMode(led_epee, OUTPUT);
   pinMode(led_foil, OUTPUT);
   pinMode(led_saber, OUTPUT);
+
+  pinMode(lhigh, OUTPUT);
+  pinMode(rhigh, OUTPUT);
+  pinMode(lgnd, OUTPUT);
+  pinMode(rgnd, OUTPUT);
 }
 
 void loop() {
+  analogWrite(lhigh, 100);
+
+  unsigned const measurement = analogRead(ltip);
+  if(measurement > signal_3v) {
+    digitalWrite(led_lhit, HIGH);
+    delay(500);
+    digitalWrite(led_lhit, LOW);
+  }
 }
