10c11f32fc5ce25d483eb6ab0d59a234637d410e
Blaine Pace <blainepace@gmail.com>
2013-03-26 01:09:05 -0700

    Mapped functions to pin numbers

--- src/fenceref.cpp
+++ src/fenceref.cpp
@@ -4,15 +4,32 @@
   #include "WProgram.h"
 #endif
 
-unsigned int led = 13;
+unsigned int led_lhit = 0;
+unsigned int led_rhit = 2;
+unsigned int led_loff = 1;
+unsigned int led_roff = 3;
+unsigned int led_epee = 11;
+unsigned int led_foil = 12;
+unsigned int led_saber = 13;
+
+unsigned int weapon_button = 8;
+
+unsigned int lhigh = 9;
+unsigned int rhigh = 10;
+unsigned int lgnd = 5;
+unsigned int rgnd = 6;
+
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led_lhit, OUTPUT);
+  pinMode(led_rhit, OUTPUT);
+  pinMode(led_loff, OUTPUT);
+  pinMode(led_roff, OUTPUT);
+
+  pinMode(led_epee, OUTPUT);
+  pinMode(led_foil, OUTPUT);
+  pinMode(led_saber, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
 }
